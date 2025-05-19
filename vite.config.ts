5b7d5fedd8e9d312eb8fb48051b2cc36a9308a86
arifomer <arifomer000@gmail.com>
2025-05-19 21:26:46 +0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-    base: "/surveyjs-demo/",
+    base: "/surveyjs-quiz-app/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
